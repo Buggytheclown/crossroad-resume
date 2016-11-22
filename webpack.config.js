2463cc7c2f204633f6b1488b9869eba36c7d5c2e
tidjei <tidjei@192.168.1.4>
2016-11-23 00:26:15 +0300

    New project structure, stage2 - it works!

--- webpack.config.js
+++ webpack.config.js
@@ -5,38 +5,56 @@ var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 var DIST_DIR = path.resolve(__dirname, "dist");
 var SRC_DIR = path.resolve(__dirname, "src");
-var ASSETS_SRC_DIR = path.resolve(__dirname, "src/assets");
-var ASSETS_DIST_DIR = path.resolve(__dirname, "dist/app/assets");
+var SRC_ASSETS_DIR = path.resolve(__dirname, "src/assets");
+var DIST_ASSETS_DIR = path.resolve(__dirname, "dist/assets");
 
 var config = {
-    entry: path.join(SRC_DIR + "/app/index.js"),
+    entry: {
+        index: path.join(SRC_DIR + "/app/index/index.js")
+    },
     output: {
         path: path.join(DIST_DIR + "/app"),
-        filename: "bundle.js",
-        publicPath: "/app/"
+        filename: "[name]/bundle.js",
+        publicPath: "/"
     },
+    // watch: true,
     devtool: "source-map",
+    devServer: {
+        // This is required for older versions of webpack-dev-server
+        // if you use absolute 'to' paths. The path should be an
+        // absolute path to your build destination.
+        contentBase: DIST_DIR,
+        // inline: true,
+        // hot: true,
+        outputPath: SRC_DIR
+    },
     module: {
         loaders: [
             {
                 test: /\.scss$/,
                 loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-            },
-            {
-                test: /\.(png|woff|woff2|eot|ttf|svg|jpg|gif)$/,
-                loader: 'file-loader?name=/images/[name].[ext]'
             }
+            // {
+            //     test: /\.(png|woff|woff2|eot|ttf|svg|jpg|gif)$/,
+            //     loader: 'file-loader?name=/images/[name].[ext]'
+            // }
         ]
     },
     // sassLoader: {
     //     includePaths: [path.resolve(__dirname, DIST_DIR)]
     // },
     plugins: [
-        new ExtractTextPlugin("styles.css"),
+        // new webpack.NoErrorsPlugin(),
+        new ExtractTextPlugin("[name]/bundle.css"),
         new CopyWebpackPlugin([
             // Copy directory contents to {output}/to/directory/
-            {from: ASSETS_SRC_DIR, to: ASSETS_DIST_DIR}
-        ])
+            {from: SRC_ASSETS_DIR, to: DIST_ASSETS_DIR},
+            {from: SRC_DIR + "/app/index/index.html", to: DIST_DIR + "/app/index"}
+        ]),
+        new webpack.DefinePlugin({
+            'NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'dev')
+        })
+
     ]
 };
 
