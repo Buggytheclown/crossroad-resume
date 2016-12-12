9e71250fe38075b47a87ab2d962c4056674296ac
tidjei <a.m.ivanov375@gmail.com>
2016-12-12 19:11:03 +0300

    improve header, navigation

--- src/app/js/index.js
+++ src/app/js/index.js
@@ -3,6 +3,9 @@
     $(function () {
         prepareSkills();
         // animateSkills();
+        addHeaderLinkEvents();
+        // addSectionChangelisteners();
+
     });
 
     function prepareSkills() {
@@ -10,7 +13,7 @@
             if (percent < 25) {
                 return '#A63D40';
             } else if (percent < 50) {
-                return '#E9B872';
+                return '#FDE74C';
             } else if (percent < 75) {
                 return '#00a8ff';
             } else if (percent <= 100) {
@@ -22,17 +25,17 @@
 
         jQuery('.skillbar').each(function () {
             var $this = jQuery(this);
-            var currentProcent = $this.attr('data-percent');
+            var currentProcent = $this.data('percent');
             $this
-                .find('.skillbar-bar, .skillbar-title')
+                .find('.skillbar__bar, .skillbar__title')
                 .css('background', chooseColor(currentProcent));
 
             $this
-                .find('.skillbar-bar')
+                .find('.skillbar__bar')
                 .css('width', currentProcent + '%');
 
             $this
-                .find('.skill-bar-percent')
+                .find('.skillbar__percent')
                 .html(currentProcent + '%');
 
         });
@@ -46,5 +49,46 @@
     //     });
     // }
 
+    function uncheckAllHeaderLink() {
+        $('.b-navigation__item').each(function () {
+            $(this).attr('data-active', 'false');
+        })
+    }
+
+    function addHeaderLinkEvents() {
+        $('.b-navigation__item').on('click', function (e) {
+            e.preventDefault();
+            var $this = $(this);
+            uncheckAllHeaderLink();
+            $this.attr('data-active', 'true');
+            var targetEl = $this.attr('data-scrollto');
+
+            $('html, body').animate({
+                scrollTop: $(targetEl).offset().top - 66
+            }, 1000);
+        })
+    }
+
+    // function addSectionChangelisteners() {
+    //     function getElementsWithPositions() {
+    //         return $('.b-navigation__item').map(function (_, el) {
+    //             var target = $(el).attr('data-scrollto');
+    //             return {element: $(target), position: $(target).offset().top};
+    //         });
+    //     }
+    //
+    //     var elementsWithPositions = getElementsWithPositions().sort(function (a, b) {
+    //         return b.position - a.position
+    //     });
+    //     var viewporHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    //
+    //     console.info(elementsWithPositions);
+    //     $(window).on('scroll', function () {
+    //
+    //
+    //         $(this).scrollTop()
+    //
+    //     })
+    // }
 
 })(jQuery);
